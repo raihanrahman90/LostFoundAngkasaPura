ffe60b6e27e24e3eb769aa4d47ba5d77523ad869
re-beat <raihanr090@gmail.com>
2023-08-21 06:32:42 +0800

    open cors

--- LostFoundAngkasaPura/Program.cs
+++ LostFoundAngkasaPura/Program.cs
@@ -108,7 +108,7 @@ builder.Services.AddCors(options =>
         {
             policy
             //.AllowAnyOrigin()
-            .WithOrigins("http://localhost:3000", "https://loclahost:3000", "http://103.150.92.47:3000")
+            .WithOrigins("http://localhost:3000", "https://loclahost:3000", "http://103.150.92.47:3001")
             //            .WithMethods("PUT", "POST", "GET","OPTIONS", "DELETE")
             .AllowAnyMethod()
             .WithHeaders("Authorization", "Content-Type", "Cookies")
